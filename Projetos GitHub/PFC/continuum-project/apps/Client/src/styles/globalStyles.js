47e7536afe0700964bd4239744d631d83e961def
Lucas Roberto <lrsm_dev.gma@outlook.com>
2026-08-29 17:52:54 -0300

    initial commit: monorepo structure

---
+++ Projetos GitHub/PFC/continuum-project/apps/Client/src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+import {createGlobalStyle} from 'styled-components'
+
+// Estilos globais para o programa, aplicados em toda a aplicação
+
+const MyGlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+export default MyGlobalStyle
