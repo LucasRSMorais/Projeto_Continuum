7edb286985136bf2faccfe2f98e756c001146ffc
Lucas Roberto <lrsm_dev.gma@outlook.com>
2026-09-13 21:09:59 -0300

    cumprimento dos requisitos e 4

--- apps/Server/api/login_paciente.php
+++ apps/Server/api/login_paciente.php
@@ -30,10 +30,8 @@ if (!isset($_SESSION['bloqueio_login'])) {
 $_SESSION['ultima_atividade'] = time();
 
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+require_once __DIR__ . "/../config/cors.php";
+applyCorsHeaders();
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
--- apps/Server/api/register_paciente.php
+++ apps/Server/api/register_paciente.php
@@ -4,9 +4,8 @@
 // Recebe dados do frontend, valida e salva o usuário no banco com senha criptografada.
 
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+require_once __DIR__ . "/../config/cors.php";
+applyCorsHeaders();
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
