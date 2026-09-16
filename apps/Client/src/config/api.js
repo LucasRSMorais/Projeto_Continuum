1c1f3107a34e6d765bf20679e501756720574f7f
Lucas Roberto <lrsm_dev.gma@outlook.com>
2026-09-16 13:17:06 -0300

    correcao no register

--- apps/Client/src/config/api.js
+++ apps/Client/src/config/api.js
@@ -2,6 +2,7 @@ export const API_BASE_URL = import.meta.env.VITE_BACKEND;
 
 export const buildApiUrl = (path = '') => {
   const cleanBase = API_BASE_URL.replace(/\/$/, '');
+  const apiBase = /\/api$/i.test(cleanBase) ? cleanBase : `${cleanBase}/api`;
   const cleanPath = path.replace(/^\//, '');
-  return `${cleanBase}/${cleanPath}`;
+  return `${apiBase}/${cleanPath}`;
 };
