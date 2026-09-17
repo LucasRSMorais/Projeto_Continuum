435ec1eff62e1e00ad25882ad854b9d38df7542e
Leo <leofelixredemption@gmail.com>
2026-09-17 15:57:12 -0300

    Requisitos Parte 5

--- apps/Server/api/verify-2fa.php
+++ apps/Server/api/verify-2fa.php
@@ -11,17 +11,16 @@ session_set_cookie_params([
 session_start();
 
 header("Content-Type: application/json; charset=UTF-8");
-<<<<<<< HEAD
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-=======
+
 require_once __DIR__ . "/../config/cors.php";
 require_once __DIR__ . "/registrarLog.php";
 require_once __DIR__ . "/../config/database.php";
-applyCorsHeaders();
->>>>>>> cbb65d9 (Atualiza sistema de logs e autenticação)
+
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
